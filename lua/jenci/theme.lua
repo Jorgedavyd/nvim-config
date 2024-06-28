94c43dd489dd70738a05928f7c4400632f21abbb
Jorgedavyd <jorged.encyso@gmail.com>
2024-06-28 15:21:46 -0400

    added more features

--- lua/jenci/theme.lua
+++ lua/jenci/theme.lua
@@ -1,5 +1,5 @@
 require("catppuccin").setup({
-    flavour = "auto", -- latte, frappe, macchiato, mocha
+    flavour = "frappe", -- latte, frappe, macchiato, mocha
     background = { -- :h background
         light = "latte",
         dark = "mocha",
@@ -8,13 +8,13 @@ require("catppuccin").setup({
     show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
     term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
-        enabled = false, -- dims the background color of inactive window
+        enabled = true, -- dims the background color of inactive window
         shade = "dark",
         percentage = 0.15, -- percentage of the shade to apply to the inactive window
     },
     no_italic = false, -- Force no italic
     no_bold = false, -- Force no bold
-    no_underline = false, -- Force no underline
+    no_underline = true, -- Force no underline
     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
         comments = { "italic" }, -- Change the style of comments
         conditionals = { "italic" },
