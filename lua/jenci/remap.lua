c55982a29f9e10c475fdca6792340f3d459e1f9c
Jorgedavyd <jorged.encyso@gmail.com>
2024-07-11 10:36:40 -0400

    added: clipboard leader

--- lua/jenci/remap.lua
+++ lua/jenci/remap.lua
@@ -17,3 +17,6 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- next greatest remap ever : asbjornHaland
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
